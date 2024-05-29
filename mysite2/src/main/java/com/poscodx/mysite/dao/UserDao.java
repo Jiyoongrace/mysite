619dbb7836a267bb5b059dac2f10c72b041e6d6c
Jiyoongrace <giyun3632@naver.com>
2024-05-29 14:53:32 +0900

    :zap: fix: login & join

--- mysite2/src/main/java/com/poscodx/mysite/dao/UserDao.java
+++ mysite2/src/main/java/com/poscodx/mysite/dao/UserDao.java
@@ -1,8 +1,12 @@
 package com.poscodx.mysite.dao;
 
-import com.poscodx.mysite.vo.UserVo;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-import java.sql.*;
+import com.poscodx.mysite.vo.UserVo;
 
 public class UserDao {
     private Connection getConnection() throws SQLException {
@@ -10,6 +14,7 @@ public class UserDao {
 
         try {
             Class.forName("org.mariadb.jdbc.Driver");
+
             String url = "jdbc:mariadb://192.168.0.195:3306/webdb?charset=utf8";
             conn = DriverManager.getConnection(url, "webdb", "webdb");
         } catch (ClassNotFoundException e) {
@@ -21,7 +26,6 @@ public class UserDao {
 
     public int insert(UserVo vo) {
         int result = 0;
-
 
         try (
                 Connection conn = getConnection();
@@ -50,78 +54,84 @@ public class UserDao {
 
         try (
                 Connection conn = getConnection();
-                PreparedStatement pstmt = conn.prepareStatement("select no, name from user where email =? and password = password(?)");
+                PreparedStatement pstmt = conn.prepareStatement("select no, name from user where email = ? and password=password(?)");
         ) {
+
             pstmt.setString(1, email);
             pstmt.setString(2, password);
 
-            try (ResultSet rs = pstmt.executeQuery()) {
-                if (rs.next()) {
-                    result = new UserVo();
-                    result.setNo(rs.getLong("no"));
-                    result.setName(rs.getString("name"));
-                    result.setEmail(rs.getString("email"));
-                    result.setGender(rs.getString("gender"));
-                    result.setJoinDate(rs.getString("join_date"));
-                }
-            } catch (SQLException e) {
-                System.out.println("error: " + e);
-            }
-        } catch (SQLException e) {
-            System.out.println("error: " + e);
-        }
+            ResultSet rs = pstmt.executeQuery();
+            if(rs.next()) {
+                Long no = rs.getLong(1);
+                String name = rs.getString(2);
 
+                result = new UserVo();
+                result.setNo(no);
+                result.setName(name);
+            }
+            rs.close();
+        } catch (SQLException e) {
+            System.out.println("Error:" + e);
+        }
 
         return result;
     }
 
-    public UserVo findByNo(Long no) {
+    public UserVo findByNo(Long userNo) {
         UserVo result = null;
 
         try (
                 Connection conn = getConnection();
-                PreparedStatement pstmt = conn.prepareStatement("select name, email, password, gender from user where no =?");
+                PreparedStatement pstmt = conn.prepareStatement("select no, name, email, gender from user where no = ?");
         ) {
-            pstmt.setLong(1, no);
 
-            try (ResultSet rs = pstmt.executeQuery()) {
-                if (rs.next()) {
-                    result = new UserVo();
-                    result.setName(rs.getString("name"));
-                    result.setEmail(rs.getString("email"));
-                    result.setPassword(rs.getString("password"));
-                    result.setGender(rs.getString("gender"));
-                }
-            } catch (SQLException e) {
-                System.out.println("error: " + e);
+            pstmt.setLong(1, userNo);
+
+            ResultSet rs = pstmt.executeQuery();
+            if(rs.next()) {
+                Long no = rs.getLong(1);
+                String name = rs.getString(2);
+                String email = rs.getString(3);
+                String gender = rs.getString(4);
+
+                result = new UserVo();
+                result.setNo(no);
+                result.setName(name);
+                result.setEmail(email);
+                result.setGender(gender);
             }
+            rs.close();
         } catch (SQLException e) {
-            System.out.println("error: " + e);
+            System.out.println("Error:" + e);
         }
+
         return result;
     }
 
-    public void updateUserNamePassword(String name, String password, String gender, Long no) {
+    public int update(UserVo vo) {
+        int result = 0;
+
         try (
                 Connection conn = getConnection();
-                PreparedStatement pstmt1 = conn.prepareStatement("update user set name =?, gender =? where no =?");
-                PreparedStatement pstmt2 = conn.prepareStatement("update user set name =?, password = password(?), gender =? where no =?");
-
+                PreparedStatement pstmt1 = conn.prepareStatement("update user set name=?, gender=? where no=?");
+                PreparedStatement pstmt2 = conn.prepareStatement("update user set name=?, password=password(?), gender=? where no=?");
         ) {
-            if("".equals(password)) {
-                pstmt1.setString(1, name);
-                pstmt1.setString(2, gender);
-                pstmt1.setLong(3, no);
-                pstmt1.executeUpdate();
+            if("".equals(vo.getPassword())) {
+                pstmt1.setString(1, vo.getName());
+                pstmt1.setString(2, vo.getGender());
+                pstmt1.setLong(3, vo.getNo());
+                result = pstmt1.executeUpdate();
             } else {
-                pstmt2.setString(1, name);
-                pstmt2.setString(2, password);
-                pstmt2.setString(3, gender);
-                pstmt2.setLong(4, no);
-                pstmt2.executeUpdate();
+                pstmt2.setString(1, vo.getName());
+                pstmt2.setString(2, vo.getPassword());
+                pstmt2.setString(3, vo.getGender());
+                pstmt2.setLong(4, vo.getNo());
+                result = pstmt2.executeUpdate();
             }
         } catch (SQLException e) {
-            System.out.println("error: " + e);
+            System.out.println("Error:" + e);
         }
+
+        return result;
     }
 }
