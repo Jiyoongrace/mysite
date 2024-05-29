c9621d65d08fa8cab3a93ad7e543dca871cc64c5
Jiyoongrace <giyun3632@naver.com>
2024-05-29 14:52:55 +0900

    :sparkles: feat: servlet init()

--- mysite2/src/main/java/com/poscodx/mysite/controller/MainServlet.java
+++ mysite2/src/main/java/com/poscodx/mysite/controller/MainServlet.java
@@ -2,8 +2,18 @@ package com.poscodx.mysite.controller;
 
 import com.poscodx.mysite.controller.action.main.MainAction;
 
+import javax.servlet.ServletException;
+
 public class MainServlet extends ActionServlet {
     private static final long serialVersionUID = 1L;
+
+    @Override
+    public void init() throws ServletException {
+        String config = getServletConfig().getInitParameter("config");
+        System.out.println("MainController.init() called: " + config);
+
+        super.init();
+    }
 
     @Override
     protected Action getAction(String actionName) {
