ff400fbb8b4f64ea7c2f728c552324c3a3128645
Jiyoongrace <giyun3632@naver.com>
2024-06-04 14:14:27 +0900

    :art: fix: spring exception handler

--- mysite3/src/main/java/com/poscodx/mysite/exception/GlobalExceptionHandler.java
+++ mysite3/src/main/java/com/poscodx/mysite/exception/GlobalExceptionHandler.java
@@ -1,17 +1,28 @@
 package com.poscodx.mysite.exception;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public String handler(Exception e) {
+    public String handler(Exception e, Model model) {
         // 1. 로깅(logging)
-        System.out.println(e);
+//        String error ="";
+//        e.printStackTrace();
+//        System.out.println(e);
+
+        StringWriter errors = new StringWriter();
+        e.printStackTrace(new PrintWriter(errors));
+        System.out.println(errors.toString());
 
         // 2. 사과(종료)
+        model.addAttribute("error", errors.toString());
         return "errors/exception";
     }
 }
