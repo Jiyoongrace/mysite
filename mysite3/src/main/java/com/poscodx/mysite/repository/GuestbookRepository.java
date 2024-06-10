47aa247d519ef5fb7e94f7f8a4959b1f4531858b
Jiyoongrace <giyun3632@naver.com>
2024-06-10 09:16:09 +0900

    :zap: fix: guestbook3 bug

--- mysite3/src/main/java/com/poscodx/mysite/repository/GuestbookRepository.java
+++ mysite3/src/main/java/com/poscodx/mysite/repository/GuestbookRepository.java
@@ -16,15 +16,19 @@ public class GuestbookRepository {
         this.sqlSession = sqlSession;
     }
 
+    public List<GuestbookVo> findAll() {
+        return sqlSession.selectList("guestbook.findAll");
+    }
+
+    public GuestbookVo findByNo(Long no) {
+        return sqlSession.selectOne("guestbook.findByNo", no);
+    }
+
     public int deleteByNoAndPassword(Long no, String password) {
         return sqlSession.delete("guestbook.deleteByNoAndPassword", Map.of("no", no, "password", password));
     }
 
     public int insert(GuestbookVo vo) {
         return sqlSession.insert("guestbook.insert", vo);
-    }
-
-    public List<GuestbookVo> findAll() {
-        return sqlSession.selectList("guestbook.findAll");
     }
 }
