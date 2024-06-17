7d097f9680f3f6a6302c40a372ffbaf7c20744b3
Jiyoongrace <giyun3632@naver.com>
2024-06-17 11:44:40 +0900

    Revert "Revert ":sparkles: feat: JsonResult""
    This reverts commit 12b4e83327faf1e3b1808df56e9677111df43c9b.

--- mysite3/src/main/java/com/poscodx/mysite/controller/api/UserController.java
+++ mysite3/src/main/java/com/poscodx/mysite/controller/api/UserController.java
@@ -1,11 +1,11 @@
 package com.poscodx.mysite.controller.api;
 
+import com.poscodx.mysite.dto.JsonResult;
 import com.poscodx.mysite.service.UserService;
 import com.poscodx.mysite.vo.UserVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
 
 @RestController("userApiController")
 @RequestMapping("/user/api")
@@ -14,11 +14,9 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    // @ResponseBody
     @GetMapping("/checkemail")
-    public Object checkEmail(@RequestParam(value="email", required = true, defaultValue="") String email) {
+    public JsonResult checkEmail(@RequestParam(value="email", required = true, defaultValue="") String email) {
         UserVo vo = userService.getUser(email);
-
-        return Map.of("exist", vo != null);
+        return JsonResult.success(vo != null);
     }
 }
--- mysite3/src/main/java/com/poscodx/mysite/service/FileUploadService.java
+++ mysite3/src/main/java/com/poscodx/mysite/service/FileUploadService.java
@@ -1,13 +1,13 @@
 package com.poscodx.mysite.service;
 
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Calendar;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class FileUploadService {
@@ -23,7 +23,7 @@ public class FileUploadService {
                 uploadDirectory.mkdirs();
             }
 
-            if(file.isEmpty()) {
+            if(file == null || file.isEmpty()) {
                 return url;
             }
 
@@ -42,9 +42,11 @@ public class FileUploadService {
             os.close();
 
             url = URL_PATH + "/" + saveFilename;
-        } catch (IOException ex) {
+
+        } catch(IOException ex) {
             throw new RuntimeException(ex);
         }
+
         return url;
     }
 
@@ -63,4 +65,5 @@ public class FileUploadService {
 
         return filename;
     }
+
 }
